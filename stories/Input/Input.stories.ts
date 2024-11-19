673cb6db5d8972b2a32453f2ae6c7cac5ee2cbad
minjeongss <dotoriido@gmail.com>
2024-11-19 23:47:31 +0900

    feat: storybook example, Input 폴더 분리
    - example: 기본으로 제공해주는 파일들 묶음
    - Input: 회원가입+로그인에서 활용되는 입력창 묶음

--- stories/Input/Input.stories.ts
+++ stories/Input/Input.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Input from "../components/Input/Input"; // 수정된 부분
+import Input from "../../components/Input/Input"; // 수정된 부분
 
 const meta = {
   title: "Input/Input",
