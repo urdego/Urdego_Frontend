c95cbe7665b87b73c31d3af2411ed695d642a3a1
chj <bluedog129@gmail.com>
2024-11-21 23:48:03 +0900

    feat: SignUpTabs stories 이벤트리스너 수정

--- stories/Layout/Login/SignUpTabs.stories.tsx
+++ stories/Layout/Login/SignUpTabs.stories.tsx
@@ -11,7 +11,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    onTabClick: (tab) => {
+    handleTabClick: (tab) => {
       console.log(`Selected tab: ${tab}`);
     },
   },
