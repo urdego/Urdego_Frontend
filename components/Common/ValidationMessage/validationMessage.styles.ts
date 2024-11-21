8278d5c50d0711c6f529820c84b926ee18b7d343
chj <bluedog129@gmail.com>
2024-11-21 23:11:24 +0900

    feat: validation message styled components

---
+++ components/Common/ValidationMessage/validationMessage.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import colors from '@styles/color/palette';
+
+export const StyledMessage = styled.p`
+  color: ${colors.alert[50]};
+  font-size: 12px;
+  margin: 0;
+  padding: 0;
+`;
