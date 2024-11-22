67f74499214501515c4efef1e9e6a46c9cd4b8a3
minjeongss <dotoriido@gmail.com>
2024-11-22 09:59:41 +0900

    feat: 전체 import 경로 수정 및 css box-sizing 적용

--- app/(nav)/home/page.tsx
+++ app/(nav)/home/page.tsx
@@ -1,28 +1,30 @@
-"use client"
-import TopBar from "@/components/Common/TopBar/TopBar";
-import { MainBanner } from "@/components/Layout/MainBanner/MainBanner";
+'use client';
+import TopBar from '@/components/Common/TopBar/TopBar';
+import { MainBanner } from '@/components/Layout/Home/MainBanner/MainBanner';
 
 import RoomButton from '@/components/Common/RoomButton/RoomButton';
 import ChannelButton from '@/components/Layout/Home/ChannelButton/ChannelButton';
 import LoactionButton from '@/components/Layout/Home/LocationButton/LocationButton';
+import { HomeWrapper } from './Home.styles';
 
 const Home = () => {
   return (
-    <>
-      <TopBar NavType="main"/>
-      <MainBanner/>
-      <h1>Home</h1>
-      <RoomButton
-        title="방제목"
-        hostUser="유저명"
-        groupMemberCount={3}
-        maxMemberCount={8}
-      />
-      <LoactionButton title="올린 장소" count={999} />
-      <ChannelButton title="그룹 게임" />
-      <ChannelButton title="랭킹 게임" />
-
-    </>
+    <div>
+      <TopBar NavType="main" />
+      <MainBanner />
+      <HomeWrapper>
+        <h1>Home</h1>
+        <RoomButton
+          title="방제목"
+          hostUser="유저명"
+          groupMemberCount={3}
+          maxMemberCount={8}
+        />
+        <LoactionButton title="올린 장소" count={999} />
+        <ChannelButton title="그룹 게임" />
+        <ChannelButton title="랭킹 게임" />
+      </HomeWrapper>
+    </div>
   );
 };
 
--- components/Layout/Home/MainBanner/MainBanner.styles.ts
+++ components/Layout/Home/MainBanner/MainBanner.styles.ts
@@ -1,16 +1,16 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const BannerWrapper = styled.div`
   position: fixed;
   top: 0;
-  width: 100%;
+  width: 430px;
   left: 50%;
   transform: translateX(-50%);
   display: flex;
   justify-content: center;
   align-items: center;
   height: 280px;
-  background-color: #F4EEFF;
+  background-color: #f4eeff;
 `;
 
 export const LevelWrapper = styled.div`
