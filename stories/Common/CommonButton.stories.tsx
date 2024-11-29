9280b52d173dd1ccd0b6352fa4bf69b83881564e
Hyesu <skxldzm123@naver.com>
2024-11-29 11:55:49 +0900

    fix: storybook 공통버튼 이슈해결

--- stories/Common/CommonButton.stories.tsx
+++ stories/Common/CommonButton.stories.tsx
@@ -1,25 +1,25 @@
-import Button from "../../components/Common/Button/Button";
-import PlusIcon from "../../styles/Icon/Plus.svg";
-import { Meta, StoryObj } from "@storybook/react";
+import Button from '../../components/Common/Button/Button';
+import PlusIcon from '../../styles/Icon/Plus.svg';
+import { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
-  title: "Common/CommonButton",
+  title: 'Common/CommonButton',
   component: Button,
   argTypes: {
     buttonType: {
-      control: "select",
-      options: ["fill", "outline"],
+      control: 'select',
+      options: ['fill', 'outline'],
     },
     buttonSize: {
-      control: "select",
-      options: ["small", "large"],
+      control: 'select',
+      options: ['small', 'large'],
     },
     buttonHeight: {
-      control: "select",
-      options: ["default", "short"],
+      control: 'select',
+      options: ['default', 'short'],
     },
     label: {
-      control: "text",
+      control: 'text',
     },
     icon: {
       control: false,
@@ -32,65 +32,65 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    buttonType: "fill",
-    buttonSize: "large",
-    label: "기본 버튼",
+    // buttonType: "fill",
+    buttonSize: 'large',
+    label: '기본 버튼',
   },
 };
 
 export const Outline: Story = {
   args: {
-    buttonType: "outline",
-    buttonSize: "large",
-    label: "테두리 버튼",
+    // buttonType: 'outline',
+    buttonSize: 'large',
+    label: '테두리 버튼',
   },
 };
 
 export const Small: Story = {
   args: {
-    buttonType: "fill",
-    buttonSize: "small",
-    label: "작은 버튼",
+    // buttonType: 'fill',
+    buttonSize: 'small',
+    label: '작은 버튼',
   },
 };
 
 export const Large: Story = {
   args: {
-    buttonType: "fill",
-    buttonSize: "large",
-    label: "큰 버튼",
+    // buttonType: 'fill',
+    buttonSize: 'large',
+    label: '큰 버튼',
   },
 };
 
 export const WithIcon: Story = {
   args: {
-    buttonType: "fill",
-    buttonSize: "large",
-    label: "장소 추가",
+    // buttonType: 'fill',
+    buttonSize: 'large',
+    label: '장소 추가',
     icon: PlusIcon,
   },
 };
 
 export const WriteButton: Story = {
   args: {
-    buttonType: "fill",
-    buttonSize: "large",
-    label: "작성완료",
+    // buttonType: 'fill',
+    buttonSize: 'large',
+    label: '작성완료',
   },
 };
 
 export const SmallOutline: Story = {
   args: {
-    buttonType: "outline",
-    buttonSize: "small",
-    label: "취소하기",
+    // buttonType: 'outline',
+    buttonSize: 'small',
+    label: '취소하기',
   },
 };
 
 export const SmallFill: Story = {
   args: {
-    buttonType: "fill",
-    buttonSize: "small",
-    label: "선택하기",
+    // buttonType: 'fill',
+    buttonSize: 'small',
+    label: '선택하기',
   },
 };
