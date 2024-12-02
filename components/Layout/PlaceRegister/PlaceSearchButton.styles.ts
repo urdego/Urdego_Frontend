f9b4c60fe3ef4af113e796f09da05a61bf42ec6a
minjeongss <dotoriido@gmail.com>
2024-12-02 12:07:30 +0900

    feat: 장소 등록하기에서 연동된 주소 재입력하는  기능 구현

--- components/Layout/PlaceRegister/PlaceSearchButton.styles.ts
+++ components/Layout/PlaceRegister/PlaceSearchButton.styles.ts
@@ -11,6 +11,23 @@ export const PlaceSearchButtonWrapper = styled.div`
   cursor: pointer;
 `;
 
+export const PlaceTexthButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4px;
+  border-radius: 4px;
+  border: 1px solid ${colors.gray[70]};
+  padding: 12px;
+`;
+
+export const PlaceResetButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
 export const LocationRegisterText = styled.div`
   color: ${colors.gray[70]};
   font-size: 16px;
--- components/Layout/PlaceRegister/PlaceSearchButton.tsx
+++ components/Layout/PlaceRegister/PlaceSearchButton.tsx
@@ -1,10 +1,13 @@
 import { useRouter } from 'next/navigation';
 import {
   PlaceSearchButtonWrapper,
+  PlaceTexthButtonWrapper,
+  PlaceResetButton,
   LocationRegisterText,
   LocationText,
 } from './PlaceSearchButton.styles';
 import { SearchIcon } from './PlaceSearchButtonIcon';
+import { ClearIcon } from './PlaceRegisterIcon';
 
 interface PlaceSearchButtonProps {
   index: number;
@@ -13,19 +16,27 @@ interface PlaceSearchButtonProps {
 
 const PlaceSearchButton = ({ index, value }: PlaceSearchButtonProps) => {
   const router = useRouter();
+
   return (
-    <PlaceSearchButtonWrapper
-      onClick={() => router.push(`/locationRegister/${index}`)}
-    >
+    <>
       {value ? (
-        <LocationText>{value}</LocationText>
+        <PlaceTexthButtonWrapper>
+          <LocationText>{value}</LocationText>
+          <PlaceResetButton
+            onClick={() => router.push(`/locationRegister/${index}`)}
+          >
+            <ClearIcon />
+          </PlaceResetButton>
+        </PlaceTexthButtonWrapper>
       ) : (
-        <>
+        <PlaceSearchButtonWrapper
+          onClick={() => router.push(`/locationRegister/${index}`)}
+        >
           <SearchIcon />
           <LocationRegisterText>위치 추가하기</LocationRegisterText>
-        </>
+        </PlaceSearchButtonWrapper>
       )}
-    </PlaceSearchButtonWrapper>
+    </>
   );
 };
 
