3fb9744f52da1cfc752e351bde835f9befab698f
minjeongss <dotoriido@gmail.com>
2024-12-03 14:26:22 +0900

    fix: PlaceRegister Page useSearchParams() 문제 해결 위해 layout에 Suspense 추가

--- app/(nav)/placeRegister/layout.tsx
+++ app/(nav)/placeRegister/layout.tsx
@@ -1,5 +1,5 @@
 import Script from 'next/script';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 export default function PlaceRegisterLayout({
   children,
@@ -7,12 +7,12 @@ export default function PlaceRegisterLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <Suspense>
       <Script
         id="google-maps-script"
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_LOCATION_REGISTER_GOOGLE_MAPS_API_KEY}&libraries=places&loading=async`}
       />
       <>{children}</>
-    </>
+    </Suspense>
   );
 }
