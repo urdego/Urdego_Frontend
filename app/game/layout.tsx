adb527cf71d3f25a29a5aa91203520790745d064
Hyesu <skxldzm123@naver.com>
2024-12-03 12:36:00 +0900

    fix: game URL 변경

--- app/game/layout.tsx
+++ app/game/layout.tsx
@@ -35,7 +35,7 @@ export default function InGameLayout({
     <>
       <Script
         id="google-maps-script"
-        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`}
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
         onLoad={handleScriptLoad}
         onError={handleScriptError}
       />
