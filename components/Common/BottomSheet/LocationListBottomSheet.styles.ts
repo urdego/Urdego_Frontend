90d65c4c2b63af8bf9cf0b50dfee3234a2b53949
minjeongss <dotoriido@gmail.com>
2024-12-08 17:31:34 +0900

    fix: motion > framer-motion으로 패키지 변경

--- components/Common/BottomSheet/LocationListBottomSheet.styles.ts
+++ components/Common/BottomSheet/LocationListBottomSheet.styles.ts
@@ -1,5 +1,5 @@
 import colors from '@/styles/color/palette';
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
 export const BackgroundOverlay = styled(motion.div)`
