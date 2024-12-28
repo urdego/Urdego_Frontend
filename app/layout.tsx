367feea0807140eaff944370a1a184fd17579c60
minjeongss <dotoriido@gmail.com>
2024-12-28 18:35:39 +0900

    feat: layout에 sessionProvider 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Container from '@/styles/Container';
 import GlobalStyle from '@/styles/GlobalStyle';
 import { Toaster } from 'react-hot-toast';
 import colors from '@styles/color/palette';
+import SessionWrapper from '@/components/Layout/Login/SessionWrapper';
 
 export { metadata };
 export { viewport };
@@ -15,29 +16,31 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body suppressHydrationWarning>
-        <StyledComponentsRegistry>
-          <GlobalStyle />
-          <Container>{children}</Container>
-          <Toaster
-            position="bottom-center"
-            toastOptions={{
-              error: {
-                duration: 2000,
-                style: {
-                  background: colors.etc.white,
-                  color: colors.etc.black,
-                  fontSize: '14px',
-                  padding: '12px 20px',
-                  borderRadius: '4px',
-                  maxWidth: '280px',
+    <SessionWrapper>
+      <html lang="ko">
+        <body suppressHydrationWarning>
+          <StyledComponentsRegistry>
+            <GlobalStyle />
+            <Container>{children}</Container>
+            <Toaster
+              position="bottom-center"
+              toastOptions={{
+                error: {
+                  duration: 2000,
+                  style: {
+                    background: colors.etc.white,
+                    color: colors.etc.black,
+                    fontSize: '14px',
+                    padding: '12px 20px',
+                    borderRadius: '4px',
+                    maxWidth: '280px',
+                  },
                 },
-              },
-            }}
-          />
-        </StyledComponentsRegistry>
-      </body>
-    </html>
+              }}
+            />
+          </StyledComponentsRegistry>
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
