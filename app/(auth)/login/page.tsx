451292558e977dca9238697a83af7ae6ec987b94
minjeongss <dotoriido@gmail.com>
2024-12-30 17:54:06 +0900

    fix: 사용하지 않는 handleToken 삭제

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -7,17 +7,6 @@ const LoginPage = () => {
   const { data: session } = useSession();
   console.log(session?.accessToken);
 
-  const handleToken = async () => {
-    const response = await fetch('/api/auth/token', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${session?.accessToken}`, // 카카오 토큰
-      },
-    });
-    const { token } = await response.json(); // 백엔드 JWT 받기
-    return token;
-  };
-
   return (
     <LoginWrapper>
       {session ? (
