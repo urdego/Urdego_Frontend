ec99724a37c333205902af71dcc5ff27a0faaa86
minjeongss <dotoriido@gmail.com>
2025-01-04 21:49:39 +0900

    feat: 비동키 컴포넌트 로딩 시 strategy 설정

--- app/(nav)/placeRegister/layout.tsx
+++ app/(nav)/placeRegister/layout.tsx
@@ -11,6 +11,7 @@ export default function PlaceRegisterLayout({
       <Script
         id="google-maps-script"
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_LOCATION_REGISTER_GOOGLE_MAPS_API_KEY}&libraries=places&loading=async`}
+        strategy="beforeInteractive"
       />
       <>{children}</>
     </Suspense>
