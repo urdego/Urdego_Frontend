b9ee31780dddf122505ff6a112a6d07902ca06c6
minjeongss <dotoriido@gmail.com>
2025-01-13 23:56:48 +0900

    feat: 올린 장소 조회에 공통 BottomSheet 적용

--- components/Common/BottomSheet/LocationListBottomSheet.styles.ts
+++ components/Common/BottomSheet/LocationListBottomSheet.styles.ts
@@ -1,70 +1,4 @@
-import colors from '@/styles/color/palette';
-import { motion } from 'framer-motion';
 import styled from 'styled-components';
-
-export const BackgroundOverlay = styled(motion.div)`
-  position: fixed;
-  top: 0;
-  left: 50%;
-
-  width: 100%;
-  min-width: 340px;
-  max-width: 430px;
-  height: 100vh;
-
-  margin: 0 auto;
-
-  z-index: 999;
-  background: rgba(0, 0, 0, 0.5);
-`;
-
-export const BottomSheet = styled(motion.div)<{ $isExpand: boolean }>`
-  position: fixed;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-
-  bottom: 0;
-  left: 50%;
-
-  width: 100%;
-  min-width: 340px;
-  max-width: 430px;
-  margin: 0 auto;
-
-  height: ${({ $isExpand }) => ($isExpand ? '100vh' : '50vh')};
-
-  background: ${colors.etc.white};
-  border-radius: ${({ $isExpand }) => ($isExpand ? '0' : '4px 4px 0px 0px')};
-  padding: 12px 0 21px 0;
-
-  will-change: transform;
-  z-index: 999;
-`;
-
-export const ContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 8px;
-  padding: 0 16px 16px 16px;
-`;
-
-export const HeaderWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-
-export const HeaderHandler = styled.div`
-  width: 40px;
-  height: 4px;
-  border-radius: 100px;
-  background: ${colors.gray[50]};
-  margin: 0 0 8px 0;
-`;
 
 export const ContentHeader = styled.div`
   color: #000;
@@ -74,12 +8,12 @@ export const ContentHeader = styled.div`
   line-height: 150%;
 `;
 
-export const ContentContainer = styled.div<{ $isExpand: boolean }>`
+export const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   width: 100%;
-  height: ${({ $isExpand }) => ($isExpand ? `88vh` : '40vh')};
+  height: 88vh; //TODO 데이터 존재할 때 높이 확인 필요
   gap: 12px;
   font-size: 14px;
   overflow-y: scroll;
@@ -88,12 +22,12 @@ export const ContentContainer = styled.div<{ $isExpand: boolean }>`
   }
 `;
 
-export const NoContentText = styled.div<{ $isExpand: boolean }>`
+export const NoContentText = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: ${({ $isExpand }) => ($isExpand ? `88vh` : '40vh')};
+  height: 40vh;
   font-size: 14px;
 `;
 
