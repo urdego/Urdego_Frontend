2d3125e5b6028172e89b5bb44ca9d882c38b5b22
Hyesu <skxldzm123@naver.com>
2025-02-02 02:15:34 +0900

    fix: GLTF 모듈 불러오는 방식 수정

--- components/Layout/Home/Character/UserCharacter.tsx
+++ components/Layout/Home/Character/UserCharacter.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense, useRef, useMemo, useEffect } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Environment } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three-stdlib';
 import { useLoader } from '@react-three/fiber';
 import * as THREE from 'three';
 
