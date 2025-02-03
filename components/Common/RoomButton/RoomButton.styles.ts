cba6608b75b887cdc69f34906f644becdac25deb
minjeongss <dotoriido@gmail.com>
2025-02-03 22:52:54 +0900

    feat: 대기방 리스트 UI 구현

--- components/Common/RoomButton/RoomButton.styles.ts
+++ components/Common/RoomButton/RoomButton.styles.ts
@@ -1,44 +1,57 @@
 import styled from 'styled-components';
-// import CardBackground from '@/styles/Icon/CardBackGround.png';
+import colors from '@/styles/color/palette';
 
-export const RoomButtonWrapper = styled.div<{ $backgroundImage: string }>`
-  width: 163px;
-  height: 139px;
+export const RoomButtonWrapper = styled.div`
+  width: 100%;
+  height: 120px;
   border-radius: 12px;
-  background-image: url(${(props) => props.$backgroundImage});
-`;
+  background-color: skyblue;
+  padding: 12px 12px 16px 12px;
 
-export const RoomButtonLayout = styled.div`
-  height: calc(100% - 0.2rem);
-  padding: 20px;
+  display: flex;
   flex-direction: column;
   justify-content: space-between;
-`;
 
-export const RoomTitle = styled.div`
-  margin-bottom: 2px;
-  color: #fdf8ff;
   font-size: 14px;
-  font-weight: 700;
+  font-weight: 400;
   line-height: 150%;
-  padding-bottom: 2px;
 `;
 
-export const RoomPerson = styled.div`
+export const RoomButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
+  height: 24px;
+
+  gap: 4px;
+`;
+
+export const Round = styled.div<{ round: number }>`
+  display: flex;
+  justify-content: center;
   align-items: center;
-  gap: 2px;
-  color: #fdf8ff;
-  font-size: 12px;
-  font-weight: 400;
+
+  width: 24px;
+  border-radius: 4px;
+
+  color: ${colors.etc.white};
+  background-color: ${(props) =>
+    props.round === 3 ? '#00b7ff' : props.round === 2 ? '#42C700' : '#FFBF00'};
+  backdrop-filter: blur(2px);
 `;
 
-export const PersonCount = styled.div`
-  color: #fdf8ff;
-  font-size: 12px;
-  font-weight: 400;
-  padding-top: 10px;
-`;
+export const Participant = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  width: 44px;
 
-export const PersonBox = styled.div``;
+  color: ${colors.etc.white};
+  border-radius: 4px;
+  background: rgba(118, 119, 119, 0.5);
+  backdrop-filter: blur(2px);
+
+  padding-left: 7.5px;
+  padding-right: 6px;
+`;
