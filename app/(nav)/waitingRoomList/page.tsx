ac20ed7ee1685ede311d7e6a4cd4956ce1999e81
minjeongss <dotoriido@gmail.com>
2025-02-04 22:19:20 +0900

    feat: 대기방 리스트 화면에 호스트 타입 추가

--- app/(nav)/waitingRoomList/page.tsx
+++ app/(nav)/waitingRoomList/page.tsx
@@ -1,14 +1,16 @@
 'use client';
+
 import TopBar from '@/components/Common/TopBar/TopBar';
 import RoomButton from '@/components/Common/RoomButton/RoomButton';
-import { PageWrapper } from '@/app/commonPage.styles';
+import { WaitingRoomListPageWrapper } from '@/app/commonPage.styles';
 import { RoomButtonGrid, ListTitle, SubTitle } from './waitingRoomList.styles';
 import { RefreshIcon } from '@/components/Common/RoomButton/WaitingRoomListIcon';
 
 const WaitingRoomList = () => {
   const roomButtons = Array.from({ length: 20 }, (_, index) => (
     <RoomButton
-      key={index}
+      key={`key${index}`}
+      hostType={'basic'}
       title={`방제목 ${index + 1}`}
       round={1}
       currMemberCount={3}
@@ -19,13 +21,13 @@ const WaitingRoomList = () => {
   return (
     <>
       <TopBar NavType="default" label="대기방" />
-      <PageWrapper>
+      <WaitingRoomListPageWrapper>
         <SubTitle>
           <ListTitle>참여 가능한 방</ListTitle>
           <RefreshIcon onClick={() => alert('OK')} />
         </SubTitle>
         <RoomButtonGrid>{roomButtons}</RoomButtonGrid>
-      </PageWrapper>
+      </WaitingRoomListPageWrapper>
     </>
   );
 };
