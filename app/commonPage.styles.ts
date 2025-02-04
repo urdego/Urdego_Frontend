a171766f1231f04a02e48d7acdaaf340b5bf9ff9
minjeongss <dotoriido@gmail.com>
2025-02-04 22:17:38 +0900

    refactor: 대기방 리스트 화면 전용 page wapper 생성

--- app/commonPage.styles.ts
+++ app/commonPage.styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import BackgroundImage from '@styles/Image/WaitingRoom/BackgroundImageL.png';
 
 export const PageWrapper = styled.div`
   width: 100%;
@@ -11,4 +12,16 @@ export const HomePageWrapper = styled.div`
   min-height: calc(100vh - 40px);
   padding: 0 0 80px 0;
   background: linear-gradient(180deg, #fff 10%, #cabeff 100%);
+`;
+
+export const WaitingRoomListPageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: calc(100vh - 40px);
+  padding: 0 16px 100px 16px;
+  background-image: url(${BackgroundImage.src});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
 `;
