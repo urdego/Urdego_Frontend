e403d3b65115546811387f83bb2036f557016791
minjeongss <dotoriido@gmail.com>
2025-02-05 23:01:49 +0900

    feat: 배경화면 purple > blue 업데이트 적용

--- app/commonPage.styles.ts
+++ app/commonPage.styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import BackgroundImage from '@styles/Image/WaitingRoom/BackgroundImageL.png';
+import BackgroundImage from '@styles/Image/WaitingRoom/waiting-r-1x.png';
 
 export const PageWrapper = styled.div`
   width: 100%;
