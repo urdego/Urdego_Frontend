6a6e8f4cdbb4dc73320a98764a2d5f531e6b12be
minjeongss <dotoriido@gmail.com>
2025-02-05 23:17:24 +0900

    fix: 호스트 이미지 자세한 위치 조정

--- components/Common/RoomButton/RoomButton.styles.ts
+++ components/Common/RoomButton/RoomButton.styles.ts
@@ -12,7 +12,7 @@ export const RoomButtonWrapper = styled.div<{
   background-image: url(${(props) => props.$hostCharacter.src});
   background-size: contain;
   background-repeat: no-repeat;
-  background-position: right bottom;
+  background-position: 60px 10px;
   padding: 12px 12px 16px 12px;
 
   display: flex;
