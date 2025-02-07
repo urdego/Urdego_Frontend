c173d7071f5e661da50e25951892a31fb6e09fdf
minjeongss <dotoriido@gmail.com>
2025-02-08 01:19:28 +0900

    feat: 장소 등록하기 화면 layout에서 React.Fragment 삭제

--- app/(nav)/placeRegister/layout.tsx
+++ app/(nav)/placeRegister/layout.tsx
@@ -1,28 +1,19 @@
 'use client';
 
 import { APIProvider } from '@vis.gl/react-google-maps';
-import Script from 'next/script';
-import React, { useState } from 'react';
 
 export default function PlaceRegisterLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState(false);
   return (
-    <>
-      <APIProvider
-        apiKey={
-          process.env
-            .NEXT_PUBLIC_LOCATION_REGISTER_GOOGLE_MAPS_API_KEY as string
-        }
-        onLoad={() => {
-          setLoading(true);
-        }}
-      >
-        {loading && <>{children}</>}
-      </APIProvider>
-    </>
+    <APIProvider
+      apiKey={
+        process.env.NEXT_PUBLIC_LOCATION_REGISTER_GOOGLE_MAPS_API_KEY as string
+      }
+    >
+      {children}
+    </APIProvider>
   );
 }
