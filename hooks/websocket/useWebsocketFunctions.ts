f227e11041fcea445cb2c3ae40df66451b6e987a
chj <bluedog129@gmail.com>
2025-02-08 01:40:55 +0900

    fix: 전역상태 관리 형식에 맞게 웹소켓 훅도 수정

--- hooks/websocket/useWebsocketFunctions.ts
+++ hooks/websocket/useWebsocketFunctions.ts
@@ -1,50 +1,47 @@
+import { useWebSocketStore } from '@/stores/useWebSocketStore';
 import { useState } from 'react';
-import { Client } from '@stomp/stompjs';
 
-export const useWebSocketFunctions = (
-  socketClientRef: React.MutableRefObject<Client | null>
-) => {
+export const useWebSocketFunctions = () => {
+  const { client, isConnected } = useWebSocketStore();
   const [subscribedRoom, setSubscribedRoom] = useState<string | null>(null);
 
-  /** 메시지 구독 */
   const subscribeToRoom = (
     roomId: string,
     onMessageReceived: (message: string) => void
   ) => {
-    if (socketClientRef.current) {
+    if (client && isConnected) {
       if (subscribedRoom === roomId) {
-        console.log(`이미 구독 중인 방: ${roomId}`);
+        console.log(`Already subscribed to room: ${roomId}`);
         return;
       }
 
       const subscriptionPath = `/urdego/sub/${roomId}`;
-      console.log(`방 구독 요청: ${subscriptionPath}`);
+      console.log(`Subscribing to room: ${subscriptionPath}`);
 
-      socketClientRef.current.subscribe(subscriptionPath, (message) => {
-        console.log(`방(${roomId}) 메시지 수신:`, message.body);
+      client.subscribe(subscriptionPath, (message) => {
+        console.log(`Message received in room ${roomId}:`, message.body);
         onMessageReceived(JSON.parse(message.body));
       });
 
       setSubscribedRoom(roomId);
     } else {
-      console.warn('웹소켓이 아직 연결되지 않았습니다.');
+      console.warn('WebSocket is not connected.');
     }
   };
 
-  /* 메시지 발행 */
   const sendMessage = (messageType: string, payload: object) => {
-    if (socketClientRef.current) {
+    if (client && isConnected) {
       const destination = `/urdego/pub/room/event`;
       const message = { messageType, payload };
 
-      socketClientRef.current.publish({
+      client.publish({
         destination,
         body: JSON.stringify(message),
       });
 
-      console.log('메시지 발행:', message);
+      console.log('Message sent:', message);
     } else {
-      console.warn('웹소켓 연결이 없습니다.');
+      console.warn('WebSocket is not connected.');
     }
   };
 
