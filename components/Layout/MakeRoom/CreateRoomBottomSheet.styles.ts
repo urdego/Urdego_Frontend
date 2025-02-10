45af2b22105ee9dfe8136eaeb5a9960b49d686c0
chj <bluedog129@gmail.com>
2025-02-10 17:09:51 +0900

    feat: 대기방 생성 로직 UI 구현

---
+++ components/Layout/MakeRoom/CreateRoomBottomSheet.styles.ts
@@ -0,0 +1,59 @@
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+import colors from '@/styles/color/palette';
+
+export const BackgroundOverlay = styled(motion.div)<{ $isOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  display: ${({ $isOpen }) => ($isOpen ? 'block' : 'none')};
+  z-index: 999;
+`;
+
+export const BottomSheetWrapper = styled(motion.div)<{ $isExpand: boolean }>`
+  position: fixed;
+  bottom: 80px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: min(100%, 430px);
+  height: 75vh;
+  background: ${colors.etc.white};
+  border-radius: 12px 12px 0 0;
+  box-shadow: 0 -4px 10px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  padding: 12px 16px;
+`;
+
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 0;
+`;
+
+export const HeaderHandler = styled.div`
+  width: 40px;
+  height: 4px;
+  border-radius: 100px;
+  background: ${colors.gray[50]};
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+  gap: 8px;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
