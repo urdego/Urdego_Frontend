64eed165683e3e35b7227399c10a6a18ccd8e0d1
minjeongss <dotoriido@gmail.com>
2025-02-11 23:02:42 +0900

    feat: 검색어 일괄 삭제한다면 장소 리스트 일괄 삭제

--- hooks/locationList/useSearchLocationList.ts
+++ hooks/locationList/useSearchLocationList.ts
@@ -38,6 +38,8 @@ const useSearchLocationList = () => {
   useEffect(() => {
     if (searchKeyword) {
       fetchSearchResult();
+    } else {
+      setLocationList([]);
     }
   }, [searchKeyword]);
 
