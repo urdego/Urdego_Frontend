802d868e7f1c6fee16127b2861756a284cfb84de
chj <bluedog129@gmail.com>
2025-02-13 15:10:01 +0900

    feat: 유저 폰트 굵기 변경

--- components/Layout/WaitingRoom/PositionCard.styles.ts
+++ components/Layout/WaitingRoom/PositionCard.styles.ts
@@ -50,7 +50,7 @@ export const Level = styled.div`
 export const Username = styled.div`
   font-size: 12px;
   font-style: normal;
-  font-weight: 700;
+  font-weight: 400;
   line-height: 150%;
   letter-spacing: -0.12px;
   color: ${colors.gray[50]};
