a30fcfda86ee7f3a35573d569ac344213ad9be7b
chj <bluedog129@gmail.com>
2025-02-16 12:27:33 +0900

    feat: notification 경로 추가

--- config/webSocketConfig.ts
+++ config/webSocketConfig.ts
@@ -2,5 +2,6 @@ export const WEBSOCKET_CONFIG = {
   SUBSCRIBE_ROOM: (roomId: string) => `/urdego/sub/${roomId}`,
   PUBLISH_ROOM_EVENT: '/urdego/pub/room/event',
   PUBLISH_GAME_EVENT: '/urdego/pub/game/event',
+  PUBLISH_NOTIFICATION_EVENT: '/urdego/pub/notification/event',
   // 추후 다른 경로들도 여기에 추가 예정
 };
