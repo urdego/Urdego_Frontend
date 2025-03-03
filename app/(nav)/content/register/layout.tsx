ef7fc70e3f69633f9db8dd3714bc2771753d2edd
minjeongss <dotoriido@gmail.com>
2025-03-03 23:37:26 +0900

    fix: 구글맵 토큰 만료로 다른 구글맵 api key 사용

--- app/(nav)/content/register/layout.tsx
+++ app/(nav)/content/register/layout.tsx
@@ -8,11 +8,7 @@ export default function ContentRegisterLayout({
   children: React.ReactNode;
 }) {
   return (
-    <APIProvider
-      apiKey={
-        process.env.NEXT_PUBLIC_LOCATION_REGISTER_GOOGLE_MAPS_API_KEY as string
-      }
-    >
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
       {children}
     </APIProvider>
   );
