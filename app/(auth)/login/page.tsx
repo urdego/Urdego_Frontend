6aa1aeb90becb1047cc373818ebce1227fe5c981
Hyesu <skxldzm123@naver.com>
2025-03-05 13:33:43 +0900

    feat: 로그인 후 상태 관리 변경(zustand->session)

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -7,34 +7,22 @@ import Image from 'next/image';
 import { LogoContainer, SocialButton } from './Login.styles';
 import KakaoLogin from '@/styles/Icon/Login/KakaoLogin.svg';
 import AppleLogin from '@/styles/Icon/Login/AppleLogin.svg';
-import useUserStore from '@/stores/useUserStore';
 import { useRouter } from 'next/navigation';
 
 const LoginPage = () => {
   const { data: session } = useSession();
-  const userId = useUserStore((state) => state.userId);
-  const nickname = useUserStore((state) => state.nickname);
-  const email = useUserStore((state) => state.email);
-  const { setEmail } = useUserStore();
+  const userId = session?.user?.userId;
+  const nickname = session?.user?.nickname;
+  const email = session?.user?.email;
   const router = useRouter();
 
   // store 값 변화 감지
   useEffect(() => {
-    console.log('로그인 페이지 - 유저 스토어 정보:', {
-      userId,
-      nickname,
-      email,
-    });
-
-    // 이메일 저장
-    if (session?.user?.email) {
-      setEmail(session.user.email);
-    }
     // 로그인된 경우 /home으로 리다이렉션
     if (session) {
       router.push('/home');
     }
-  }, [userId, nickname, session, router, email, setEmail]);
+  }, [userId, nickname, email, session, router]);
 
   return (
     <LoginWrapper>
