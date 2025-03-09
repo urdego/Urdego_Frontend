6d1f1547e91c468572c293cbfc68de0ce0da3f92
chj <bluedog129@gmail.com>
2025-03-10 01:33:43 +0900

    feat: 대기방 리스트에서 대기방 선택하여 들어가기 구현

--- components/Common/RoomButton/RoomButtonList.tsx
+++ components/Common/RoomButton/RoomButtonList.tsx
@@ -1,11 +1,19 @@
 import { WaitingRoomList } from '@/hooks/waitingRoomList/useGetWaitingRoomList';
 import RoomButton from './RoomButton';
+import { useRouter } from 'next/navigation';
 
 const RoomButtonList = ({
   waitingRoomList,
 }: {
   waitingRoomList: WaitingRoomList[] | null;
 }) => {
+  const router = useRouter();
+
+  // 방 이동 함수
+  const handleRoomClick = (roomId: string) => {
+    router.push(`game/${roomId}/waitingRoom`);
+  };
+
   return (
     <>
       {waitingRoomList &&
@@ -17,6 +25,7 @@ const RoomButtonList = ({
             round={item.totalRounds}
             currMemberCount={item.currentPlayersCount}
             maxMemberCount={item.maxPlayers}
+            onClick={() => handleRoomClick(item.roomId)}
           />
         ))}
     </>
