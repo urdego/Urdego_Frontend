4fce4528ea579f17614aa940d252dff314da6565
chj <bluedog129@gmail.com>
2025-03-10 13:19:49 +0900

    feat: 대기방 리스트에서 대기방으로 들어가기 전 방ID 전역 저장

--- components/Common/RoomButton/RoomButtonList.tsx
+++ components/Common/RoomButton/RoomButtonList.tsx
@@ -1,6 +1,7 @@
 import { WaitingRoomList } from '@/hooks/waitingRoomList/useGetWaitingRoomList';
 import RoomButton from './RoomButton';
 import { useRouter } from 'next/navigation';
+import useGameStore from '@/stores/useGameStore';
 
 const RoomButtonList = ({
   waitingRoomList,
@@ -8,9 +9,11 @@ const RoomButtonList = ({
   waitingRoomList: WaitingRoomList[] | null;
 }) => {
   const router = useRouter();
+  const { setRoomId } = useGameStore();
 
   // 방 이동 함수
   const handleRoomClick = (roomId: string) => {
+    setRoomId(roomId);
     router.push(`game/${roomId}/waitingRoom`);
   };
 
