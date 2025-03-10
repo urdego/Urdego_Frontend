15a90ace8b96d163eeb299d589c8d1bde95b2821
chj <bluedog129@gmail.com>
2025-03-10 15:17:01 +0900

    feat: 방 나가기 이벤트 메시지 보내기 구현

--- app/game/[roomId]/waitingRoom/page.tsx
+++ app/game/[roomId]/waitingRoom/page.tsx
@@ -20,6 +20,7 @@ import useUserStore from '@/stores/useUserStore';
 import { useWebSocketFunctions } from '@/hooks/websocket/useWebsocketFunctions';
 import { RoomPayload } from '@/lib/types/roomJoin';
 import { useRouter } from 'next/navigation';
+import { is } from '@react-three/fiber/dist/declarations/src/core/utils';
 
 const WaitingRoom = () => {
   const [isAddContentsVisible, setIsAddContentsVisible] = useState(false);
@@ -157,6 +158,11 @@ const WaitingRoom = () => {
 
   // 방 나가기 시 구독 해제 후 홈으로 이동
   const handleExit = useCallback(() => {
+    sendMessage(
+      'PLAYER_REMOVE',
+      { roomId: String(roomId), userId: Number(userId) },
+      'room'
+    );
     isLeaving.current = true;
     unsubscribeFromRoom(String(roomId));
     router.push('/home');
