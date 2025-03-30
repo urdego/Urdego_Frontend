2a20bfe886a8792935d1052c39867645e1a1e505
chj <bluedog129@gmail.com>
2025-03-31 04:21:49 +0900

    fix: authOptions 파일에 auth 로직 이동 route는 라우터 기능만 하도록 수정

--- app/(nav)/myPage/page.tsx
+++ app/(nav)/myPage/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth/authOptions';
 import MyPageClient from '@/app/(nav)/myPage/MyPageClient';
 import { API_URL_CONFIG } from '@/config/apiEndPointConfig';
 import axiosInstance from '@/lib/axios';
