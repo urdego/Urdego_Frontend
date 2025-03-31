6cdf97b4f47e020e6d9aaef6599edd05705a4fbe
chj <bluedog129@gmail.com>
2025-03-31 17:27:56 +0900

    Refactor: 불 필요한 주석 제거

--- app/(nav)/myPage/page.tsx
+++ app/(nav)/myPage/page.tsx
@@ -6,14 +6,12 @@ import { API_URL_CONFIG } from '@/config/apiEndPointConfig';
 import axiosInstance from '@/lib/axios';
 
 export default async function MyPage() {
-  // 서버에서 세션 정보를 가져옴
   const session = await getServerSession(authOptions);
   if (!session) {
     console.error('세션이 없습니다. 로그인 페이지로 리다이렉트합니다.');
     redirect('/login');
   }
 
-  // 세션에서 userId 추출
   const userId = session.user?.userId;
   if (!userId) {
     console.error(
@@ -23,16 +21,11 @@ export default async function MyPage() {
   }
 
   try {
-    console.log(`서버 컴포넌트에서 userId: ${userId}로 직접 백엔드 API 호출`);
-    console.log(`호출 URL: ${API_URL_CONFIG.USER_SERVICE.USERS}/${userId}`);
-
-    // 라우터를 거치지 않고 직접 백엔드 API 호출
     const response = await axiosInstance.get(
       `${API_URL_CONFIG.USER_SERVICE.USERS}/${userId}`
     );
 
     const { email, nickname, activeCharacter, exp, level } = response.data;
-    console.log('서버에서 받아온 userData:', response.data);
 
     const userData = {
       email,
@@ -42,7 +35,6 @@ export default async function MyPage() {
       exp,
     };
 
-    // 클라이언트 컴포넌트로 데이터를 전달
     return <MyPageClient userData={userData} />;
   } catch (error) {
     console.error('서버사이드 에러:', error);
