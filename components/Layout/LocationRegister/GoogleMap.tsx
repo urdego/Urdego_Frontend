2dcb7a391ef9771885fcc5c26354b46307382b63
minjeongss <dotoriido@gmail.com>
2025-04-14 00:57:12 +0900

    refactor: TBT 테스트 위해 최상위에서 Provider 호출

--- components/Layout/LocationRegister/GoogleMap.tsx
+++ components/Layout/LocationRegister/GoogleMap.tsx
@@ -1,7 +1,6 @@
 import { AdvancedMarker, Map, MapMouseEvent } from '@vis.gl/react-google-maps';
 import Image from 'next/image';
 import useCharacterMarker from '@/hooks/character/useCharacterMarker';
-import { APIProvider } from '@vis.gl/react-google-maps';
 
 interface GoogleMapProps {
   isLocationSelected: boolean;
@@ -22,31 +21,29 @@ const GoogleMap = ({ markerPosition, setMarkerPosition }: GoogleMapProps) => {
   };
 
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
-      <Map
-        mapId={'LocationRegisterPage'}
-        style={{ height: `calc(100vh - 40px)` }}
-        defaultCenter={{ lat: 36.5, lng: 127.5 }}
-        defaultZoom={8}
-        gestureHandling={'greedy'}
-        disableDefaultUI={true}
-        onClick={handleMapClick}
-      >
-        {markerPosition.lat !== 0 && markerPosition.lng !== 0 && (
-          <AdvancedMarker
-            position={{ lat: markerPosition.lat, lng: markerPosition.lng }}
-            clickable={true}
-          >
-            <Image
-              src={getMyMarkerIcon()}
-              width={50}
-              height={53}
-              alt="UserMarker Icon"
-            />
-          </AdvancedMarker>
-        )}
-      </Map>
-    </APIProvider>
+    <Map
+      mapId={'LocationRegisterPage'}
+      style={{ height: `calc(100vh - 40px)` }}
+      defaultCenter={{ lat: 36.5, lng: 127.5 }}
+      defaultZoom={8}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+      onClick={handleMapClick}
+    >
+      {markerPosition.lat !== 0 && markerPosition.lng !== 0 && (
+        <AdvancedMarker
+          position={{ lat: markerPosition.lat, lng: markerPosition.lng }}
+          clickable={true}
+        >
+          <Image
+            src={getMyMarkerIcon()}
+            width={50}
+            height={53}
+            alt="UserMarker Icon"
+          />
+        </AdvancedMarker>
+      )}
+    </Map>
   );
 };
 
